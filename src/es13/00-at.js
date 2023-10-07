48d10896d86d76ae056f94b98b148cba52dd27fc
Andrés Felipe Bolaños <ghostafbr@gmail.com>
2023-10-07 15:06:02 -0500

    Es13

---
+++ src/es13/00-at.js
@@ -0,0 +1,4 @@
+const array = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
+console.log(array.at(0)); // one
+console.log(array.at(-1)); // ten
+console.log(array[array.length] -1 ); // undefined
